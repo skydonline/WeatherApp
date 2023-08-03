ce316a1f8ab7f0a77682dd8d5c2897752878f10a
Sky Deng <sky.deng.online@outlook.com>
2023-08-03 18:29:10 -0400

    working fetching from usegetweather

--- App.js
+++ App.js
@@ -7,56 +7,21 @@ import { WEATHER_API_KEY } from "@env";
 import { useGetWeather } from "./src/hooks/useGetWeather";
 
 export default function App() {
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-    const [weather, setWeather] = useState([]);
-    const [lat, setLat] = useState([]);
-    const [lon, setLon] = useState([]);
-
-    const fetchWeatherData = async () => {
-        try {
-            const res = await fetch(
-                `http://api.weatherapi.com/v1/forecast.json?key=${WEATHER_API_KEY}&q=${lat},${lon}&days=3&aqi=no&alerts=no`
-            );
-            const data = await res.json();
-            setWeather(data);
-        } catch (e) {
-            setError("Unable to fetch weather.");
-        } finally {
-            setLoading(false);
-        }
-    };
-
-    useEffect(() => {
-        (async () => {
-            let { status } = await Location.requestForegroundPermissionsAsync();
-            if (status !== "granted") {
-                setError("Location services was denied");
-                return;
-            }
-            let location = await Location.getCurrentPositionAsync({});
-            setLat(location.coords.latitude);
-            setLon(location.coords.longitude);
-            await fetchWeatherData();
-        })();
-    }, [lat, lon]);
+    const [loading, error, weather] = useGetWeather();
+    console.log(loading, error, weather);
 
     if (weather) {
-        console.log(weather);
-    }
-
-    if (loading) {
         return (
-            <View style={styles.container}>
-                <ActivityIndicator size={"large"} color={"lightblue"} />
-            </View>
+            <NavigationContainer>
+                <Tabs weather={weather} />
+            </NavigationContainer>
         );
     }
 
     return (
-        <NavigationContainer>
-            <Tabs />
-        </NavigationContainer>
+        <View style={styles.container}>
+            <ActivityIndicator size={"large"} color={"lightblue"} />
+        </View>
     );
 }
 
