bcd1d4c5c13bdfc5c91a18ddd2cb637346b10c69
Sky Deng <sky.deng.online@outlook.com>
2023-08-04 10:39:50 -0400

    wokring upcoming weather

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -10,15 +10,19 @@ import {
 } from "react-native";
 import { Feather } from "@expo/vector-icons";
 import { weatherType } from "../utilities/weatherType";
+import moment from "moment";
 
 export default function ListItem(props) {
-    const { date, min, max, condition } = props;
+    const { dt_txt, min, max, condition } = props;
     console.log(`The condition is: ${condition}`);
-    const { item, dateStyles, temp } = styles;
+    const { item, dateStyles, temp, dateTextWrapper } = styles;
     return (
         <View style={item}>
             <Feather name={weatherType[condition].icon} size={50} color={"white"} />
-            <Text style={dateStyles}>{date}</Text>
+            <View style={dateTextWrapper}>
+                <Text style={dateStyles}>{moment(dt_txt).format("dddd")}</Text>
+                <Text style={dateStyles}>{moment(dt_txt).format("MMM Do YY")}</Text>
+            </View>
             <Text style={temp}>{`${Math.round(min)}°/${Math.round(max)}°`}</Text>
         </View>
     );
@@ -42,6 +46,9 @@ const styles = StyleSheet.create({
     dateStyles: {
         color: "white",
         fontSize: 50,
+    },
+    dateTextWrapper: {
+        flexDirection: "column",
     },
     image: {
         flex: 1,
